6e68e481e0ae3d3831ed02fdfd314f3827da8d18
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-04-09 16:32:49 +0200

    more

--- 12ax.py
+++ 12ax.py
@@ -1,34 +1,112 @@
 #!/usr/bin/python
 
-lastNum = ""
-lastLetter = ""
-def nextWantedOutput(nextInput):
-        global lastNum, lastLetter
+class SeqGenerator:
+    def __init__(self):
+        self.lastNum = self.lastLetter = ""
+
+    def peek(self, nextInput):
         if nextInput in ["1","2"]:
-                lastNum = nextInput
-                lastLetter = ""
-                return "L"
+            return "L"
         elif nextInput in ["A","B"]:
-                lastLetter = nextInput
-                return "L"
+            return "L"
         elif nextInput in ["X","Y"]:
-                seq = lastNum + lastLetter + nextInput
-                lastLetter = nextInput
-                if seq in ["1AX","2BY"]: return "R"
-                return "L"
+            seq = self.lastNum + self.lastLetter + nextInput
+            if seq in ["1AX","2BY"]: return "R"
+            return "L"
         return None
 
-def nextWantedOutputs(nextInputs):
-        return [ nextWantedOutput(c) for c in nextInputs ]
+    def next(self, nextInput):
+        out = self.peek(nextInput)
+        if nextInput in ["1","2"]:
+            self.lastNum = nextInput
+            self.lastLetter = ""
+        elif nextInput in ["A","B","X","Y"]:
+            self.lastLetter = nextInput
+        return out
 
+    def nextSeq(self, nextInputs):
+        return [ self.next(c) for c in nextInputs ]
+
+    def nextStr(self, nextInputs):
+        return "".join([ self.next(c) or "" for c in nextInputs ])
+
+def seqStr(s): return SeqGenerator().nextStr(s)
 
 import pybrain
-import pybrain.tools.shortcuts as pys
-from pybrain.structure.modules import BiasUnit, SigmoidLayer, LinearLayer, LSTMLayer
+import pybrain.tools.shortcuts as bs
+from pybrain.structure.modules import BiasUnit, SigmoidLayer, LinearLayer, LSTMLayer, SoftmaxLayer
+import pybrain.structure.networks as bn
+import pybrain.structure.connections as bc
+import pybrain.rl.learners.valuebased as bl
+import pybrain.supervised.trainers.rprop as bt
+import pybrain.datasets.sequential as bd
 
-nn = pys.buildNetwork(9, 9, 2, hiddenclass=LSTMLayer)
+#nn = bs.buildNetwork(9, 9, 2, hiddenclass=LSTMLayer)
 
+print "preparing network ...",
+nn = bn.RecurrentNetwork()
+nn.addInputModule(LinearLayer(9, name="in"))
+nn.addModule(LSTMLayer(9, name="hidden"))
+nn.addOutputModule(SoftmaxLayer(2, name="out"))
+nn.addConnection(bc.FullConnection(nn["in"], nn["hidden"], name="c1"))
+nn.addConnection(bc.FullConnection(nn["hidden"], nn["out"], name="c2"))
+nn.addRecurrentConnection(bc.FullConnection(nn["hidden"], nn["hidden"], name="c3"))
+nn.sortModules()
+print "done"
 
+import random
 
+def getRandomSeq(seqlen):
+    s = ""
+    count = 0
+    gen = SeqGenerator()
+    for i in xrange(seqlen):
+        if(float(count) / (i+1) < random.uniform(0.0,0.2)):
+            if gen.lastNum + gen.lastLetter == "1A": c = "X"
+            elif gen.lastNum + gen.lastLetter == "2B": c = "Y"
+            elif gen.lastNum == "1": c = "A"
+            elif gen.lastNum == "2": c = "B"
+            else: c = random.choice("12")
+        else:
+            c = random.choice("123ABCXYZ")
+        s += c
+        if gen.next(c) == "R": count += 1
+    return s
 
+import pybrain.utilities
+def inputAsVec(c): return pybrain.utilities.one_to_n("123ABCXYZ".index(c), 9)
+def outputAsVec(c):
+    if c is None: return (0.0,0.0)
+    else: return pybrain.utilities.one_to_n("LR".index(c), 2)
 
+def addSequence(dataset, seqlen):
+    dataset.newSequence()
+    s = getRandomSeq(seqlen)
+    for i,o in zip(s, SeqGenerator().nextSeq(s)):
+        dataset.addSample(inputAsVec(i), outputAsVec(o))
+
+def generateData(seqlen = 100, nseq = 20):
+    dataset = bd.SequentialDataSet(9, 2)
+    for i in xrange(nseq): addSequence(dataset, seqlen)
+    return dataset
+
+#l = bl.QLambda()
+trainer = bt.RPropMinusTrainer(module=nn)
+
+from pybrain.tools.validation import testOnSequenceData
+
+import thread
+def userthread():
+    from IPython.Shell import IPShellEmbed
+    ipshell = IPShellEmbed()
+    ipshell()
+thread.start_new_thread(userthread, ())
+
+# carry out the training
+for i in xrange(100):
+    trndata = generateData()
+    tstdata = generateData()
+    trainer.trainOnDataset(trndata)
+    trnresult = 100. * (1.0-testOnSequenceData(nn, trndata))
+    tstresult = 100. * (1.0-testOnSequenceData(nn, tstdata))
+    print "train error: %5.2f%%" % trnresult, ",  test error: %5.2f%%" % tstresult
